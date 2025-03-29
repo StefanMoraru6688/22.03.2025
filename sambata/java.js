8460cf04ca081f1b97fc3b7438a4bd2e80e9b495
StefanMoraru6688 <stefanmoraru557@gmail.com>
2025-03-29 10:27:41 +0200

    sambata

---
+++ sambata/java.js
@@ -0,0 +1,14 @@
+function jucatori(){
+    var goluri = 10;
+    var nume ="Theo";
+    var div = Element.div;
+    
+}
+
+ function cluburi (){
+    var goluri = 20;
+    if(goluri<20)
+    {
+
+    }
+ }
